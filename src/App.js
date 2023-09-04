e4f1a1ab7660e69af7ba84aa53c9c10f7f5da970
Luis <LuisDEsquivel11@gmail.com>
2023-09-04 16:16:10 -0500

    Pure react components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed)
+    ]);
+};
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me!")
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Roxy",
+                breed: "Pitbull"
+            }),
+            React.createElement(Pet,{
+                animal: "Dog",
+                name: "Havoc",
+                breed: "American Bulldog"
+            }),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Penelope",
+                breed: "Egyptian"
+            })
+        ]
     )
 };
 
